4478745589c235da4c9614f11332b973f779e0c4
Graham Bennett <graham@grahambennett.org>
2022-10-20 14:54:19 +0100

    Add Nix flake for nix develop

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "LLVM";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+    };
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      inputsFrom = [
+        pkgs.clang_14.cc
+      ];
+      packages = with pkgs; [
+        ninja
+      ];
+    };
+  };
+}
