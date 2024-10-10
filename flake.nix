ed73a170ddd94f2455b2bac5538b3ad77cb8db07
Jeff Snyder <jeff-git@caffeinated.me.uk>
2024-10-10 20:05:48 +0100

    [do not merge] enable flake & customise for docker

--- flake.nix
+++ flake.nix
@@ -6,32 +6,23 @@
   };
 
   outputs = { self, nixpkgs }: let
-    #system = "x86_64-linux";
-    system = "aarch64-darwin";
+    system = "aarch64-linux";
     pkgs = import nixpkgs {
       inherit system;
     };
-    frameworks = pkgs.darwin.apple_sdk.frameworks;
-    #pkgsCross = import nixpkgs {
-    #  inherit system;
-    #  crossSystem = {
-    #    config = "aarch64-unknown-linux-gnu";
-    #  };
-    #};
   in {
     devShells.${system}.default = pkgs.mkShell {
-#      inputsFrom = [
-#        pkgs.clang_14.cc
+      inputsFrom = [
+        pkgs.clang_14.cc
 #        pkgs.clang_14.lldb
-#      ];
+      ];
       packages = with pkgs; [
         cmake
         ninja
+        clang_14
 #        python3
 #        libxml2
-        frameworks.CoreServices
       ]
-#      ++ lib.optional stdenv.hostPlatform.isDarwin fixDarwinDylibNames
       ;
     };
   };
